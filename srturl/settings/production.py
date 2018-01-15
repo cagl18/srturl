1b9a343f0bea7c03d0d1e108a2fc5a8f11f4e151
Carlos Gonzalez <cagl18@gmail.com>
2018-01-15 12:14:51 -0500

    asd

--- srturl/settings/production.py
+++ srturl/settings/production.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'srturl.urls'
 ROOT_HOSTCONF = 'srturl.hosts'
 DEFAULT_HOST = 'www' 
 DEFAULT_REDIRECT_URL = 'srturl.herokuapp.com'#'http://www.srturl.com' #srturl
-#PARENT_HOST = 'srturl.herokuapp.com'#'srturl.com'
+PARENT_HOST = 'srturl.herokuapp.com'#'srturl.com'
 
 TEMPLATES = [
     {
