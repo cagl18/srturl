a387c6c855eaa789a38f0bff0f8086755e322e66
Carlos Gonzalez <cagl18@gmail.com>
2018-01-15 10:46:40 -0500

    changing settings file

--- srturl/settings/__init__.py
+++ srturl/settings/__init__.py
@@ -1,11 +1,15 @@
 from .production import *
-
+settings = "production"
 try:
 	from .local import *
+	settings = "local"
 except:
 	pass
 
 try:
 	from .carlos_settings import *
+	settings = "carlos_settings"
 except:
 	pass
+
+print("Settings file used: -->",settings)
