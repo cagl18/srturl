469d384a7ed52d43ee66ed9f639e3040118d1a54
Carlos Gonzalez <cagl18@gmail.com>
2018-01-15 09:37:32 -0500

    another one

--- srturl/settings/production.py
+++ srturl/settings/production.py
@@ -58,9 +58,9 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'srturl.urls'
 ROOT_HOSTCONF = 'srturl.hosts'
-DEFAULT_HOST = 'www'
-DEFAULT_REDIRECT_URL = 'http://srturl.herokuapp.com'#'http://www.srturl.com' #srturl
-#PARENT_HOST = 'srturl.herokuapp.com'#'srturl.com'
+DEFAULT_HOST = 'www' 
+DEFAULT_REDIRECT_URL = 'srturl.herokuapp.com'#'http://www.srturl.com' #srturl
+PARENT_HOST = 'srturl.herokuapp.com'#'srturl.com'
 
 TEMPLATES = [
     {
